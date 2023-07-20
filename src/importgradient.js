84a9c15b87dffe94656f8b956996f9614d9345ff
Andrei Ilinescu <andreiilinescu19@gmail.com>
2023-07-21 01:56:12 +0300

    Updated Background + cursor

---
+++ src/importgradient.js
@@ -0,0 +1,5 @@
+import { Gradient } from './Gradient.js'
+// Create your instance
+const gradient = new Gradient()
+// Call `initGradient` with the selector to your canvas
+gradient.initGradient('#gradient-canvas')
